2552688a201d71d2ac47484fc7149914260d8016
Mateus Ferreira <mateusferreira0704@gmail.com>
2025-08-01 20:34:08 -0300

    feat: add search controller model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'search#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :search, only: [:index, :create] do
+    collection do
+      get :analytics
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
